245e1d8c01d8114daae7ca6482e072dd00c9c577
frankzheng <frank.zheng@liteon.com>
2024-10-08 17:14:53 +0800

    test

---
+++ PythonAPI/examples/autorun.sh
@@ -0,0 +1,83 @@
+python bus_case.py -yaw 0.70392736
+sleep 10
+python bus_case.py -yaw 1.047230757
+sleep 10
+python bus_case.py -yaw 0.982827265
+sleep 10
+python bus_case.py -yaw 0.976339717
+sleep 10
+# python bus_case.py -yaw 3
+# sleep 1
+# python bus_case.py -yaw 3.5
+# sleep 1
+# python bus_case.py -yaw 4
+# sleep 1
+# python bus_case.py -yaw 4.5
+# sleep 1
+# python bus_case.py -yaw 5
+# sleep 1
+# python bus_case.py -yaw 5.5
+# sleep 1
+# python bus_case.py -yaw 6
+# sleep 1
+# python bus_case.py -yaw 6.5
+# sleep 1
+# python bus_case.py -yaw 7
+# sleep 1
+# python bus_case.py -yaw 7.5
+# sleep 1
+# python bus_case.py -yaw 8
+# sleep 1
+# python bus_case.py -yaw 8.5
+# sleep 1
+# python bus_case.py -yaw 9
+# sleep 1
+# python bus_case.py -yaw 9.5
+
+# python bus_case.py -yaw 75
+# python bus_case.py -yaw 90
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# python around_collecting.py -classtype "walker" -distance 1
+# sleep 10
+# python around_collecting.py -classtype "walker" -distance 3
+# sleep 10
+# python around_collecting.py -classtype "walker" -distance 5
+# sleep 10
+# python around_collecting.py -classtype "walker" -distance 7
+# sleep 10
+# python around_collecting.py -classtype "motor" -distance 1
+# sleep 10
+# python around_collecting.py -classtype "motor" -distance 3
+# sleep 10
+# python around_collecting.py -classtype "motor" -distance 5
+# sleep 10
+# python around_collecting.py -classtype "motor" -distance 7
+# sleep 10
+# python around_collecting.py -classtype "bike" -distance 1
+# sleep 10
+# python around_collecting.py -classtype "bike" -distance 3
+# sleep 10
+# python around_collecting.py -classtype "bike" -distance 5
+# sleep 10
+# python around_collecting.py -classtype "bike" -distance 7
+# sleep 10
+
+
+# python bus_case.py -yaw 75
+# python bus_case.py -yaw 90
